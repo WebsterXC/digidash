fd1b2c330f04446ae8fbf01d9578104e0675f542
Mark Grassi <markgras@buffalo.edu>
2016-12-11 15:47:43 -0500

    Made some changes while testing in car.

--- src/can/socketrun.py
+++ src/can/socketrun.py
@@ -7,36 +7,38 @@ def send_recv(cmd, respSize): #send cmd parameter and return dongle response (ig
     sock.send(cmd + "\r\n")
     time.sleep(0.005)
     resp1 = ""
-    if respSize == 0:  #if 0, expected response size not known
-        resp1 = sock.recv(32)
-        resp1 = resp1.replace('\r', "")
-        resp1 = resp1.replace('>', "")
-        if resp1 == "SEARCHING...":
-            continue
-        '''if resp1 == "?":
-            self.log.info(''.join(("Command ", cmd, " is invalid.")))
-            raise InvalidCmdError("Command '%s' is invalid." % cmd)
-        if resp1 == "NO DATA":
-            self.log.info(''.join(("Command ", cmd, " produced NO DATA.")))
-            raise NoDataError("Dongle returned 'NO DATA'.")
-        if resp1 == "STOPPED":
-            self.log.info(''.join(("ELM returned STOPPED")))
-            raise StoppedError("Dongle returned 'STOPPED'.")
-        if resp1 == "UNABLE TO CONNECT":
-            self.log.warning(''.join(("ELM returned UNABLE TO CONNECT")))
-            raise StoppedError("Dongle returned 'UNABLE TO CONNECT'") '''
-        return resp1
+    while(1):
+        if respSize == 0:  #if 0, expected response size not known
+            resp1 = sock.recv(32)
+            resp1 = resp1.replace('\r', "")
+            resp1 = resp1.replace('>', "")
+            if resp1 == "SEARCHING...":
+                continue
+            '''if resp1 == "?":
+                self.log.info(''.join(("Command ", cmd, " is invalid.")))
+                raise InvalidCmdError("Command '%s' is invalid." % cmd)
+            if resp1 == "NO DATA":
+                self.log.info(''.join(("Command ", cmd, " produced NO DATA.")))
+                raise NoDataError("Dongle returned 'NO DATA'.")
+            if resp1 == "STOPPED":
+                self.log.info(''.join(("ELM returned STOPPED")))
+                raise StoppedError("Dongle returned 'STOPPED'.")
+            if resp1 == "UNABLE TO CONNECT":
+                self.log.warning(''.join(("ELM returned UNABLE TO CONNECT")))
+                raise StoppedError("Dongle returned 'UNABLE TO CONNECT'") '''
+            return resp1
+        else:
+            break
     while(1):
         resp2 = sock.recv(32)
         resp2 = resp2.replace('\r', "")
         resp2 = resp2.replace('>', "")
-        resp2 = " " + resp2
         resp1 += resp2
         data = resp1.split()
         if len(data) == 1:
             if data[0] == "SEARCHING...":
                 continue
-        '''    if data[0] == "?":
+            '''if data[0] == "?":
                 self.log.info(''.join(("Command ", cmd, " is invalid.")))
                 raise InvalidCmdError("Command '%s' is invalid." % cmd)
             if data[0] == "STOPPED":
@@ -46,13 +48,13 @@ def send_recv(cmd, respSize): #send cmd parameter and return dongle response (ig
                 print("How did I get here? Cmd was '"+cmd+"' and response was '"+resp1+"'.")
                 return resp1
         elif len(data) == 2:
-        '''    if resp1 == "NO DATA":
+            '''if resp1 == "NO DATA":
                 self.log.info(''.join(("Command ", cmd, " produced NO DATA.")))
                 raise NoDataError("Dongle returned 'NO DATA'.") '''
             if respSize == 2:
                 return resp1
         elif len(data) == 3:
-        '''    if resp1 == "UNABLE TO CONNECT":
+            '''if resp1 == "UNABLE TO CONNECT":
                 self.log.info(''.join(("ELM returned UNABLE TO CONNECT")))
                 raise StoppedError("Dongle returned 'UNABLE TO CONNECT'") '''
             if respSize == 3:
@@ -63,7 +65,7 @@ def send_recv(cmd, respSize): #send cmd parameter and return dongle response (ig
             print("How did I get here? Cmd was '"+cmd+"' and response was '"+resp1+"'.")
             '''self.log.info(''.join(("ELM returned too large of a response")))
             raise ResponseError("Dongle returned too large of a response")'''
-            return "We screwed up."
+            return "40 10 00 00"
             
 
 #myMAC = "00:1D:A5:00:03:4E" #Mark's dongle (ELM v1.5 aka shit chinese clone)
@@ -99,7 +101,7 @@ print(res)
 
 while 1:
     res = send_recv("010C", 4)
-	print("RPM response is:")
+    print("RPM response is:")
     print(res)
     data = res.split()
     a = int(data[2], 16)
@@ -107,6 +109,5 @@ while 1:
     rpm = ((256 * a) + b) / 4
     print("RPM is:")
     print(rpm)
-    time.sleep(1)
 
 sock.close()
